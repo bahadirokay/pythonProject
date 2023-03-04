a33d888eb9ffbfa3fc05fc43406b4d3b9bc09043
bahadirokay <37798554+bahadirokay@users.noreply.github.com>
2023-03-04 19:38:41 +0300

    Update dbastrokay.py

--- dbastrokay.py
+++ dbastrokay.py
@@ -2,11 +2,11 @@ import sqlite3
 
 def create_database():
 
-    # create the database file and tables
+
     db_file = "astrophotography.db"
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
-    # Create tables for each button
+
     c.execute('''CREATE TABLE IF NOT EXISTS LightFrame
             (filename TEXT, path TEXT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS DarkFrame
@@ -28,54 +28,34 @@ def create_database():
     c.execute('''CREATE TABLE IF NOT EXISTS MasterBias
             (filename TEXT, path TEXT)''')
 
-    # Commit the changes and close the connection
+
     conn.commit()
     conn.close()
 
 
 def add_data(table, file_info):
-    # establish connection to the database
+
     connection = sqlite3.connect("astrophotography.db")
     cursor = connection.cursor()
 
     try:
-        # insert the file info into the specified table
+
         cursor.execute(f"INSERT INTO {table} (filename, path) VALUES (?, ?)", file_info)
         connection.commit()
         print("Data added successfully.")
     except sqlite3.Error as e:
         print(f"Error adding data: {e}")
     finally:
-        # close the database connection
+
         connection.close()
 
 
 
 def get_data(table_name):
-    """
-    Returns all the data in the given table.
 
-    Args:
-    - table_name (str): name of the table to fetch data from.
-
-    Returns:
-    - data (list): list of tuples containing the data.
-    """
     conn = sqlite3.connect("astrophotography.db")
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM {table_name}")
     data = cur.fetchall()
     conn.close()
     return data
-
-
-connection = sqlite3.connect("astrophotography.db")
-cursor = connection.cursor()
-
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-print(cursor.fetchall())
-
-cursor.execute("PRAGMA table_info(FlatFrame)")
-print(cursor.fetchall())
-
-connection.close()
